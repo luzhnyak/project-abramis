8983e5eeff9053a3aa3b8eb3541e1e8e060e13fc
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-09-02 01:58:48 +0300

    Update modal-book.js

--- src/js/modal-book.js
+++ src/js/modal-book.js
@@ -1,7 +1,1 @@
-import { BookshelfApiService } from './api-service';
 
-const bookshelfApiService = new BookshelfApiService();
-
-const modalBtn = document.querySelector('.js-test-modal');
-
-// modalBtn.addEventListener('click', onClick);
