b9c8706ea564280814cdd027f251071ad79b9bdf
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-01 16:40:44 +0300

    dark theme for header with JS, part 1

--- src/js/theme.js
+++ src/js/theme.js
@@ -1,21 +1,48 @@
 
 const body = document.querySelector('body')
-const switcher = document.querySelector('.theme')
+
+const header = {
+ generalHeader: document.querySelector('.header'),
+ logo: document.querySelector('.logo'),
+ toggle: document.querySelector('.switch'),
+ switcher: document.querySelector('.theme')
+}
+
+const menuLinks = document.querySelectorAll('.menu-link')
+
+const headerElements = Object.values(header)
+ 
 
 function onLigth() {
     body.classList.remove('body-dark');
+
+for (const el of headerElements){
+  el.classList.remove('dark')
+}
+for (const link of menuLinks){
+  link.classList.remove ('dark')
+}
+
 }
   
   function onDark(e){
     if(e.currentTarget.checked){
-      body.classList.add("body-dark");
+      body.classList.add('body-dark');
+      
+for (const el of headerElements){
+  el.classList.add('dark')
+}
+for (const link of menuLinks){
+  link.classList.add ('dark')
+}
+
+
     }
 
     else{
       onLigth()
     }
-    // body.className = "";
    
   }
   
-  switcher.addEventListener('change', onDark)
+  header.switcher.addEventListener('change', onDark)
