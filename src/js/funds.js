a033a041ac97273343e21928a01d63953740e149
Oleh Luzhniak <oleg.luzhnyak@gmail.com>
2023-09-02 20:35:35 +0300

    scroll fix

--- src/js/funds.js
+++ src/js/funds.js
@@ -1,12 +1,12 @@
-import { fundsData } from "./funds-data";
+import { fundsData } from './funds-data';
 
 const fundsContainer = document.querySelector('.js-funds');
 const fundsRender = document.querySelector('.funds-list');
 
-
 const markup = fundsData
-    .map((fund) =>
-        `<li class="funds-item slide">
+  .map(
+    fund =>
+      `<li class="funds-item slide">
         <a class="funds-link" href="${fund.url}" target="_blank" rel="noopener noreferrer">
         <img class="funds-image" 
           src="${fund.img}" 
@@ -14,11 +14,11 @@ const markup = fundsData
           target="_parent">
          </a>
     </li> 
-        `)
-    .join("");
+        `
+  )
+  .join('');
 
-
-fundsRender.insertAdjacentHTML("beforeend", markup);
+fundsRender.insertAdjacentHTML('beforeend', markup);
 
 const down = document.querySelector('.down-scroll');
 const up = document.querySelector('.up-scroll');
@@ -29,36 +29,33 @@ up.addEventListener('click', upClick);
 let size = 0;
 
 function onClick() {
-
-    if (size > fundsRender.clientHeight) {
-        downClick();
-        size = 0;
-        down.classList.add('visually-hidden');
-        up.classList.remove('visually-hidden');
-        return;
-    } else {
-        size += fundsRender.clientHeight;
-        downClick();
-    }
-    
+  if (size > fundsRender.clientHeight) {
+    downClick();
+    size = 0;
+    down.classList.add('visually-hidden');
+    up.classList.remove('visually-hidden');
+    return;
+  } else {
+    size += fundsRender.clientHeight;
+    downClick();
+  }
 }
 
 function downClick() {
-    fundsRender.scrollTo({
-        top: fundsRender.scrollWidth = size,
-        behavior: 'smooth'
-    });
+  fundsRender.scrollTo({
+    top: fundsRender.scrollWidth,
+    behavior: 'smooth',
+  });
 }
 
 function upClick() {
-    down.classList.remove('visually-hidden');
-    up.classList.add('visually-hidden');
-    fundsRender.scrollTo({
-        top: fundsRender.scrollIntoView,
-        behavior: 'smooth'
-    });
+  down.classList.remove('visually-hidden');
+  up.classList.add('visually-hidden');
+  fundsRender.scrollTo({
+    top: fundsRender.scrollIntoView,
+    behavior: 'smooth',
+  });
 }
-
 
 // // console.log(firstEl);
 // firstEl.classList.add('active');
