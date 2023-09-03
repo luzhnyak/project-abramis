d68f4a16cce54589f2bda0021e1618b2b4a6829b
Illia-Os <113521393+Illia-Os@users.noreply.github.com>
2023-09-03 11:59:12 +0300

    add mobile menu
    an adaptive desktop and tablet destroys the mobile version and vice versa
    mobile destroys
    tablet and desktop

--- src/js/mobile-menu.js
+++ src/js/mobile-menu.js
@@ -1,1 +1,22 @@
 // alert("test")
+
+document.addEventListener('DOMContentLoaded', function () {
+  const toggleButton = document.getElementById('toggle-menu');
+  const mobileMenu = document.getElementById('mobile-menu');
+  const closeMenuButton = document.getElementById('close-menu');
+  const body = document.body;
+
+  toggleButton.addEventListener('click', function () {
+    mobileMenu.style.right = '0px';
+    toggleButton.style.display = 'none';
+    closeMenuButton.style.display = 'block';
+    body.style.overflow = 'hidden';
+  });
+
+  closeMenuButton.addEventListener('click', function () {
+    mobileMenu.style.right = '-100%';
+    toggleButton.style.display = 'block';
+    closeMenuButton.style.display = 'none';
+    body.style.overflow = 'auto';
+  });
+});
