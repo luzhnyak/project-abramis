2c8c3c2bac633415f9d83d3daaaac326dcd19787
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-09-03 21:54:53 +0300

    modify shopping list

--- src/js/shopping-list.js
+++ src/js/shopping-list.js
@@ -27,23 +27,25 @@ function cardBook({
 }) {
   return `<li class="shopping-list-book-item">
         <img class="shop-list-item-img" src="${book_image}" alt="${book_image}" width="100" height="142" />  
-        <div class="shop-list-description-wrapper">      
+      <div class="shop-list-description-wrapper">      
         <h3 class="shop-list-item-title">${title}</h3>
         <p class="shop-list-item-category">${list_name}</p>
         <p class="shop-list-item-description">${description}</p>
-        <p>${author}</p>
-        <ul class="buy-list list">
-          <li class="buy-item">
-            <a href="${buy_links[0].url}"><img class="store-1" src="${store1}" alt="" width="" height=""/></a>
-          </li>
-          <li class="buy-item">
-            <a href="${buy_links[1].url}"><img class="store-2" src="${store2}" alt="" width="" height=""/></a>
-          </li>
-          <li class="buy-item">
-            <a href="${buy_links[2].url}"><img class="store-3" src="${store3}" alt="" width="" height=""/></a>
-          </li>
-        </ul>
+        <div class="buy-item-wrapper">
+          <p class="shop-list-item-author">${author}</p>
+          <ul class="buy-list-shop list">
+            <li class="buy-item">
+              <a href="${buy_links[0].url}"><img class="store-1" src="${store1}" alt="" width="" height=""/></a>
+            </li>
+            <li class="buy-item">
+              <a href="${buy_links[1].url}"><img class="store-2" src="${store2}" alt="" width="" height=""/></a>
+            </li>
+            <li class="buy-item">
+              <a href="${buy_links[2].url}"><img class="store-3" src="${store3}" alt="" width="" height=""/></a>
+            </li>
+          </ul>
         </div>
+      </div>
       </li>`;
 }
 
