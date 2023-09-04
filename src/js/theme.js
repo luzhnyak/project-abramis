5f9b7bb49cc427e6dbf02338fc1940f7db0f14d5
Anna Boichuk <anna.boichuk.o@gmail.com>
2023-09-04 17:36:53 +0300

    dark styles toggle for mobile menu

--- src/js/theme.js
+++ src/js/theme.js
@@ -13,12 +13,9 @@ const header = {
  generalHeader: document.querySelector('.header'),
  logo: document.querySelector('.logo'),
  toggle: document.querySelector('.switch'),
- switcher: document.querySelector('.theme')
+ switcher: document.querySelector('.theme'),
+ switcherMobile: document.querySelector('.theme-mobile')
 }
-
-const menuLinks = document.querySelectorAll('.menu-link')
-
-const headerElements = Object.values(header)
 
 
 
@@ -34,16 +31,19 @@ function currentTheme(){
   const item = localStorage.getItem(STORAGE_KEY)
   const parsedItem = JSON.parse(item)
   if (parsedItem === null) {
+    header.switcherMobile.checked = false
+    header.switcher.checked = false
   }
   else{
     header.switcher.checked = true
+    header.switcherMobile.checked = true
     onDark() 
   }
 
 }
 function savedTheme(){
 const data = {}
-if(header.switcher.checked){
+if(header.switcher.checked || header.switcherMobile.checked){
   data.theme = 'dark';
 } 
 localStorage.setItem(STORAGE_KEY, JSON.stringify(data))
@@ -67,15 +67,10 @@ function onLigth() {
 */
   function onDark(e){
     savedTheme()
-
       body.classList.add('body-dark');
       
     }
     
-
-
-
-
 
     /**
       |============================
@@ -85,14 +80,29 @@ function onLigth() {
 
  function toggle(){
   if(header.switcher.checked){
-
     onDark()
-
+    header.switcherMobile.checked = true
   } 
   else{
     localStorage.clear()
     onLigth()
+    header.switcherMobile.checked = false
+  }
+ }
+
+ function toggleMobile(){
+
+  if(header.switcherMobile.checked){
+    onDark()
+    header.switcher.checked = true
+  } 
+  else{
+    localStorage.clear()
+    onLigth()
+    header.switcher.checked = false
+    
   }
  }
 
   header.switcher.addEventListener('change', toggle)
+  header.switcherMobile.addEventListener('change', toggleMobile)
