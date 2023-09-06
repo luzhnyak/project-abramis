69414d9c40e8c206b58cdd2c1e0712efd2285552
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-09-06 13:18:11 +0300

    fix: scroll uo

--- src/js/scroll-up.js
+++ src/js/scroll-up.js
@@ -1,32 +1,14 @@
-document.addEventListener('DOMContentLoaded', function () {
-  let scrollToTopBtn = document.querySelector('.scroll-to-top');
+window.addEventListener('scroll', scrollFunction);
+const scrollToTopBtn = document.querySelector('.scroll-to-top');
 
-  window.onscroll = function () {
-    scrollFunction();
-  };
-
-  function scrollFunction() {
-    if (
-      document.body.scrollTop > 20 ||
-      document.documentElement.scrollTop > 20
-    ) {
-      scrollToTopBtn.style.display = 'block';
-    } else {
-      scrollToTopBtn.style.display = 'none';
-    }
+function scrollFunction() {
+  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+    scrollToTopBtn.style.display = 'flex';
+  } else {
+    scrollToTopBtn.style.display = 'none';
   }
+}
 
-  scrollToTopBtn.addEventListener('click', function () {
-    smoothScrollToTop();
-  });
-
-  function smoothScrollToTop() {
-    let currentScroll =
-      document.documentElement.scrollTop || document.body.scrollTop;
-
-    if (currentScroll > 0) {
-      window.requestAnimationFrame(smoothScrollToTop);
-      window.scrollTo(0, currentScroll - currentScroll / 15);
-    }
-  }
+scrollToTopBtn.addEventListener('click', () => {
+  window.scrollTo({ top: 0, behavior: 'smooth' });
 });
