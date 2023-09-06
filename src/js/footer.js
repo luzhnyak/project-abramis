88b111bf70822d2e330d838445c4d22afa36d0c8
AnnMatsarska <127693004+AnnMatsarska@users.noreply.github.com>
2023-09-06 12:00:11 +0300

    edit: cards overlay for mobile and laptop is hidden

--- src/js/footer.js
+++ src/js/footer.js
@@ -1,39 +1,38 @@
 import * as basicLightbox from 'basiclightbox';
 import iconClose from '../images/x-close.png';
 
-const btnHeart = document.querySelector('.btn-footer')
+const btnHeart = document.querySelector('.btn-footer');
 
 function disableBodyScroll() {
-    document.body.style.overflow = 'hidden';
-  }
-  
-  function enableBodyScroll() {
-    document.body.style.overflow = 'visible';
-  }
+  document.body.style.overflow = 'hidden';
+}
 
-  const instance = basicLightbox.create(`<div class="modal-team"></div>`, {
-    onShow: () => {
-      document.addEventListener('keydown', onEscDown);
-      disableBodyScroll();
-    },
-    onClose: () => {
-      document.removeEventListener('keydown', onEscDown);
-      enableBodyScroll();
-    },
-  });
+function enableBodyScroll() {
+  document.body.style.overflow = 'visible';
+}
 
-   function markupTeam() {
- 
-    instance.show();
-  
-    const instanceEl = document.querySelector('.modal-team');
-  
-    instanceEl.innerHTML = `<div class="modal modal-our-team container">
+const instance = basicLightbox.create(`<div class="modal-team"></div>`, {
+  onShow: () => {
+    document.addEventListener('keydown', onEscDown);
+    disableBodyScroll();
+  },
+  onClose: () => {
+    document.removeEventListener('keydown', onEscDown);
+    enableBodyScroll();
+  },
+});
+
+function markupTeam() {
+  instance.show();
+
+  const instanceEl = document.querySelector('.modal-team');
+
+  instanceEl.innerHTML = `<div class="modal modal-our-team container">
     <section class="our-team">
       <button class="modal-close-btn" type="button" aria-label="close">
         <img class="modal-close-svg" src="${iconClose}" alt="" width="24" height="24" loading="lazy"/>
       </button>
-       <h2 class = "title-team">Team <span class = "title-shark">Shark-JS</span></h2>
+       <h2 class = "title-team">Team <span class = "title-shark">JS Sharks</span></h2>
       <ul class="list team-list">
       <li class = "team-item">
       <h3 class = "team-name">Oleh Luzhniak</h3>
@@ -75,20 +74,19 @@ function disableBodyScroll() {
     </section>
     </div>
     `;
-  
-   
-    const closeBtn = document.querySelector('.modal-close-btn');
-    closeBtn.addEventListener('click', onClickbBtn);
-  }
-  
-  function onClickbBtn(event) {
+
+  const closeBtn = document.querySelector('.modal-close-btn');
+  closeBtn.addEventListener('click', onClickbBtn);
+}
+
+function onClickbBtn(event) {
+  instance.close();
+}
+
+function onEscDown(event) {
+  if (event.code === 'Escape') {
     instance.close();
   }
-  
-  function onEscDown(event) {
-    if (event.code === 'Escape') {
-      instance.close();
-    }
-  }
+}
 
-  btnHeart.addEventListener('click', markupTeam)
+btnHeart.addEventListener('click', markupTeam);
